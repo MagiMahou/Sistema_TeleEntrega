f4a6e7803cf6f2155774970314e64dc253aaae90
Gabriel Tavares <136284534+MagiMahou@users.noreply.github.com>
2026-05-25 16:44:34 -0300

    Atualizado calculo do imposto

--- src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Dados/ImpostoServiceFixo.java
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Dados/ImpostoServiceFixo.java
@@ -3,11 +3,19 @@ package com.bcopstein.ex4_lancheriaddd_v1.Adaptadores.Dados;
 import org.springframework.stereotype.Service;
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Servicos.IImpostoService;
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.ItemPedido;
 
 @Service
 public class ImpostoServiceFixo implements IImpostoService {
     @Override
     public double calcularImposto(Pedido pedido) {
-        return pedido.getValor() * 0.10;
+        double subtotal = 0.0;
+        
+        if (pedido.getItens() != null) {
+            for (ItemPedido item : pedido.getItens()) {
+                subtotal += (item.getProduto().getPreco() * item.getQuantidade());
+            }
+        }
+        return subtotal * 0.10;
     }
 }
